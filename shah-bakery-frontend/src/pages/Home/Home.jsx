3d5370b45d080751bbb3c429289452796d162f26
MOHAMMAD-HAFIZ-SHAH <mohammadhafiz.shah.23cse@bmu.edu.in>
2026-05-29 23:24:12 +0530

    Adding admin frontend flow

--- shah-bakery-frontend/src/pages/Home/Home.jsx
+++ shah-bakery-frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,24 @@
 import MainLayout from "../../layouts/MainLayout";
-import Hero from "../../components/home/Hero";
+
+import Hero from "./Hero";
+import Categories from "./Categories";
+import FeaturedProducts from "./FeaturedProducts";
+import Outlets from "./Outlets";
+import Testimonials from "./Testimonials";
 
 const Home = () => {
   return (
     <MainLayout>
 
       <Hero />
+
+      <Categories />
+
+      <FeaturedProducts />
+
+      <Outlets />
+
+      <Testimonials />
 
     </MainLayout>
   );
---
+++ shah-bakery-frontend/src/pages/Menu/Menu.jsx
@@ -0,0 +1,32 @@
+import MainLayout from "../../layouts/MainLayout";
+
+import MenuHero from "./MenuHero";
+import CategorySidebar from "./CategorySidebar";
+import ProductGrid from "./ProductGrid";
+import FloatingCart from "../../components/cart/FloatingCart";
+
+const Menu = () => {
+  return (
+    <MainLayout>
+
+      <MenuHero />
+
+      <section className="py-20 bg-[var(--cream)]">
+
+        <div className="container-custom grid lg:grid-cols-[280px_1fr] gap-12">
+
+          <CategorySidebar />
+
+          <ProductGrid />
+
+        </div>
+
+      </section>
+
+      <FloatingCart />
+
+    </MainLayout>
+  );
+};
+
+export default Menu;
